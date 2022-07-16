950cdfd3080894a9d8560f9de9d5f4ec2623bba0
rreone2017 <rreone2017@gmail.com>
2022-07-16 16:46:52 +0900

    make ticket sale application ex1

---
+++ src/test/java/com/object1_ex1/ticket2/Ticket2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.object1_ex1.ticket2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ticket2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
